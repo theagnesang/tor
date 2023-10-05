550f86177585642b0f6d4129a3c57285f8f0dc75
Agnes Ang <agnesagf0@gmail.com>
2023-10-05 10:13:22 +0800

    Update onboarding feature

--- server.js
+++ server.js
@@ -74,61 +74,70 @@ global.fetch = fetch;
 const unsplashAccess = createApi({ accessKey: process.env.UNSPLASH_ACCESS_KEY });
 
 app.get("/", async (req, res) => {
-    try {
-        const unsplashImage = await unsplashAccess.search.getPhotos({ query: "creator" });
-        const randomIndex = Math.floor(Math.random() * unsplashImage.response?.results.length);
-        const photoURL = unsplashImage.response?.results[randomIndex].urls.raw;
-        const photoAltText = unsplashImage.response?.results[randomIndex].alt_description;
-        res.render("index.ejs", {
-            photo: photoURL,
-            photoAlt: photoAltText
-        });
-    }
-    catch (error) {
-        console.error("Failed to make request:", error.message);
-        res.status(500).send("Failed to fetch activity.");
-    }
-});
-
-app.get('/signup', async (req, res) => {
-    try {
-        const unsplashImage = await unsplashAccess.search.getPhotos({ query: "creator" });
-        const randomIndex = Math.floor(Math.random() * unsplashImage.response?.results.length);
-        const photoURL = unsplashImage.response?.results[randomIndex].urls.raw;
-        const photoAltText = unsplashImage.response?.results[randomIndex].alt_description;
-        res.render("create account.ejs", {
-            photo: photoURL,
-            photoAlt: photoAltText
-        });
-    }
-    catch (error) {
-        console.error("Failed to make request:", error.message);
-        res.status(500).send("Failed to fetch activity.");
-    }
+  if(req.isAuthenticated()){
+      res.redirect("/feed");
+  } else {
+      try {
+          const unsplashImage = await unsplashAccess.search.getPhotos({ query: "creator" });
+          const randomIndex = Math.floor(Math.random() * unsplashImage.response?.results.length);
+          const photoURL = unsplashImage.response?.results[randomIndex].urls.raw;
+          const photoAltText = unsplashImage.response?.results[randomIndex].alt_description;
+          res.render("index.ejs", {
+              photo: photoURL,
+              photoAlt: photoAltText
+          });
+      }
+      catch (error) {
+          console.error("Failed to make request:", error.message);
+          res.status(500).send("Failed to fetch activity.");
+      }
+  }
 });
 
 app.get('/onboarding', (req, res) => {
     res.render("onboarding.ejs");
 });
 
-app.post('/user-signup', async (req, res) => {
-    try {
-        const lowerCaseEmail = req.body.email.toLowerCase();
-        const existingUser = await db.any('SELECT * FROM auth.users WHERE email ILIKE $1', [lowerCaseEmail]);
-        
-        if(existingUser.length > 0){
-            return res.status(400).send("Email already exists");
-        }
+app.get('/user-signup', async (req, res) => {
+  if(req.isAuthenticated()){
+      res.redirect("/feed");
+  } else {
+      try {
+          const unsplashImage = await unsplashAccess.search.getPhotos({ query: "creator" });
+          const randomIndex = Math.floor(Math.random() * unsplashImage.response?.results.length);
+          const photoURL = unsplashImage.response?.results[randomIndex].urls.raw;
+          const photoAltText = unsplashImage.response?.results[randomIndex].alt_description;
+          res.render("create account.ejs", {
+              photo: photoURL,
+              photoAlt: photoAltText
+          });
+      }
+      catch (error) {
+          console.error("Failed to make request:", error.message);
+          res.status(500).send("Failed to fetch activity.");
+      }
+  }
+});
 
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(req.body.pwd, saltRounds);
-        await db.none('INSERT INTO auth.users(email, passwordhash,status) VALUES($1, $2, $3)', [req.body.email, hashedPassword, 'Active']);
-        res.status(201).send('User created successfully');
-        
-    } catch (error) {
-        console.error("Error during registration:", error);
-        res.status(500).send("Internal Server Error");
-    }
+
+app.post('/user-signup', async (req, res) => {
+  try {
+      const lowerCaseEmail = req.body.email.toLowerCase();
+      const existingUser = await db.any('SELECT * FROM auth.users WHERE email ILIKE $1', [lowerCaseEmail]);
+      
+      if(existingUser.length > 0){
+          return res.status(400).send("Email already exists");
+      }
+
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(req.body.pwd, saltRounds);
+      await db.none('INSERT INTO auth.users(email, passwordhash,status) VALUES($1, $2, $3)', [req.body.email, hashedPassword, 'Active']);
+      res.status(201).send('User created successfully');
+      
+  } catch (error) {
+      console.error("Error during registration:", error);
+      res.status(500).send("Internal Server Error");
+  }
 });
 
 app.post('/user-login', (req, res, next) => {
@@ -155,6 +164,34 @@ app.get('/feed', (req, res) => {
         res.redirect("/");
     }
 });
+
+app.post('/onboarding', async (req, res) => {
+  if (!req.isAuthenticated()) {
+      return res.status(403).send('You must be logged in to complete onboarding');
+  }
+  
+  try {
+      const data = req.body; // Your onboarding data from the client
+      const userId = req.user.userid; // Assuming the userid is stored in req.user when the user is logged in
+      
+      // Check if user already has a profile
+      const existingProfile = await db.oneOrNone('SELECT * FROM auth.user_profile WHERE userid = $1', [userId]);
+      
+      if (existingProfile) {
+          // Update existing profile with new data
+          await db.none('UPDATE auth.user_profile SET first_name = $1, last_name = $2, country = $3, content_type_id = $4 WHERE id = $5', [data.first_name, data.last_name, data.country, data.content_type_id], userId);
+      } else {
+          // Insert new profile with data
+          await db.none('INSERT INTO auth.user_profile (id, first_name, last_name, country, content_type_id) VALUES ($1, $2, $3, $4, $5)', [userId, data.first_name, data.last_name, data.country, data.content_type_id]);
+      }
+      
+      res.status(200).send('Onboarding data saved successfully');
+  } catch (error) {
+      console.error('Error during onboarding:', error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
